90be25715a8f850e6226401a8adbec8b1cef9d88
Ryan Ng <121779670+ryanng2008@users.noreply.github.com>
2024-04-22 09:52:29 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+        fontFamily: {
+          times: ['Times New Roman', 'serif'],
+          roboto: ['Roboto', 'sans']
+        },
+    },
+  },
+  plugins: [],
+}
